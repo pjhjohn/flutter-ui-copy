e2e8ac145b3401dc2d8b6d383a309ef205ef28c2
pjhjohn <pjhjohn@gmail.com>
2020-12-13 03:18:25 +0900

    Layout Sketch with Tide Background Image

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class TideUiCopyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.white, fontSize: 36.0, fontWeight: FontWeight.w400),
+          headline2: TextStyle(color: Colors.white, fontSize: 48.0, fontWeight: FontWeight.w100),
+          bodyText1: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.w300),
+        ),
       ),
       home: MainScreen(),
     );
@@ -20,15 +24,79 @@ class TideUiCopyApp extends StatelessWidget {
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenPadding = MediaQuery.of(context).padding;
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/images/sunset.jpeg'),
+            image: AssetImage('assets/images/tide.jpg'),
             fit: BoxFit.cover,
           ),
         ),
-        child: null,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: EdgeInsets.only(top: screenPadding.top),
+                child: Column(
+                  children: [
+                    Container(
+                      color: Colors.pink.withAlpha(64),
+                      height: 32.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(children: [
+                        Text('Night', style: Theme.of(context).textTheme.headline1),
+                      ]),
+                    ),
+                    Container(
+                      color: Colors.pink.withAlpha(64),
+                      height: 8.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 20.0, right: 60.0),
+                      child: Row(children: [
+                        Expanded(
+                          child: Text('Sleep is like a cat: it only comes to you if you ignore it',
+                              style: Theme.of(context).textTheme.bodyText1),
+                        )
+                      ]),
+                    ),
+                    Container(
+                      color: Colors.pink.withAlpha(64),
+                      height: 16.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(children: [
+                        Text('Daily Tide', style: Theme.of(context).textTheme.bodyText1),
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(bottom: screenPadding.bottom),
+                child: Container(
+                  color: Colors.pink.withAlpha(64),
+                  height: 160.0,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: Text('Storm', style: Theme.of(context).textTheme.headline2),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
